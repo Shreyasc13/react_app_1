9e83bf5620f16553206367c048b2beffbd3fa6b1
Shreyasc13 <44752893+Shreyasc13@users.noreply.github.com>
2021-04-25 19:38:09 +0530

    First commit

---
+++ src/robots.js
@@ -0,0 +1,39 @@
+export const robots = [{
+        id: 1,
+        name: 'shreyas',
+        username: 'shreyas',
+        email: 'shreyas@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'joe',
+        username: 'joebiden',
+        email: 'joe@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'hugh',
+        username: 'hughjackman',
+        email: 'hugh@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'robert',
+        username: 'rdj',
+        email: 'rdj@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'kevin',
+        username: 'kevin',
+        email: 'kevin@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'leon',
+        username: 'leonpatron',
+        email: 'leon@gmail.com'
+    },
+
+
+]
